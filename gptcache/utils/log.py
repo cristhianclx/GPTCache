c02a27b36baa10d28f4c66e8cf8f9f71d0dea718
Cristhian <cristhianclx@gmail.com>
2023-07-10 23:50:23 -0500

    update log format

--- gptcache/utils/log.py
+++ gptcache/utils/log.py
@@ -2,7 +2,4 @@ import logging
 
 import gptcache
 
-FORMAT = '%(asctime)s - %(thread)d - %(filename)s-%(module)s:%(lineno)s - %(levelname)s: %(message)s'
-logging.basicConfig(format=FORMAT)
-
 gptcache_log = logging.getLogger(f'gptcache:{gptcache.__version__}')
